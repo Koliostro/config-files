e3795376b11d232581ec0d3423a33b53befb5ca2
Koliostro <senior.starkoff2018@yandex.ru>
2025-07-26 14:26:46 +0300

    Added disply for battery status

--- tmuxconf/.tmux/scripts/right_var.sh
+++ tmuxconf/.tmux/scripts/right_var.sh
@@ -8,7 +8,17 @@ function current_time() {
     date "+%H : %M"
 }
 
+function battary_charge() {
+    cat /sys/class/power_supply/BAT1/capacity | xargs echo -n && echo -n "% |"
+}
+
+function is_charged() {
+    cat /sys/class/power_supply/BAT1/status | xargs echo -n && echo -n ' |'
+}
+
 function main() {
+    is_charged
+    battary_charge
     working_time
     current_time
 }
